9a8c42c68e82eb90ed59c2614eb79bba745d08e1
Albert Montané <amotane@userzoom.com>
2017-06-23 09:11:43 +0200

    Script

--- commit.sh
+++ commit.sh
@@ -1,4 +1,7 @@
 export pwd=`pwd`
-sftp s18d0439@ftp.s18d0439.alojamientovirtual.com 
-git status --porcelain | grep chapters | sed 's#.*files\/chapters\(\/.*\).*#sftp s18d0439@ftp.s18d0439.alojamientovirtual.com put'$pwd'/\/files\/chapters\/\1 public_html\/elvater.net\/morti\/files\/chapters\/\1#'
-git status --porcelain | grep additional | sed 's#.*files\/additional\(\/.*\).*#sftp s18d0439@ftp.s18d0439.alojamientovirtual.com put'$pwd'/\/files\/additional\/\1 public_html\/elvater.net\/morti\/files\/additional\/\1#'
+echo FTP FIRST
+git status --porcelain | grep chapters | sed "s#.*files/chapters\(/.*\).*#sftp s18d0439@ftp.s18d0439.alojamientovirtual.com:public_html/elvater.net/morti/files/chapters <<< $'put "$pwd"/files/chapters\1'#" | sh
+git status --porcelain | grep additional | sed "s#.*files/additional\(/.*\).*#sftp s18d0439@ftp.s18d0439.alojamientovirtual.com:public_html/elvater.net/morti/files/additional <<< $'put "$pwd"/files/additional\1'#" | sh
+echo GIT SECOND
+git add .
+git commit -m 'Commit generated automatically from $COMPUTER_ID'
